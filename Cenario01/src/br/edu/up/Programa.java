62a83efec4cec7be6a873fa78cd00705fe07efc4
RafaGutowski <162761689+RafaGutowski@users.noreply.github.com>
2024-04-22 09:44:42 -0300

    Getters e Setters
    Pegar e atribuir informações nos livros.

--- Cenario01/src/br/edu/up/Programa.java
+++ Cenario01/src/br/edu/up/Programa.java
@@ -1,6 +1,11 @@
-package Cenario01.src.br.edu.up;
-
+package br.edu.up;
+import br.edu.up.models.Livro;
 
 public class Programa {
+
+    public static void main(String[] args) {
+        
+
+    }
 
 }
--- Cenario01/src/br/edu/up/models/Livro.java
+++ Cenario01/src/br/edu/up/models/Livro.java
@@ -2,4 +2,56 @@ package br.edu.up.models;
 
 public class Livro {
 
+    private String  titulo;
+    private String codigo;
+    private String autores;
+    private String isbn;
+    private int ano;
+
+    
+    public Livro(String titulo){
+        this.titulo = titulo;
+    }
+
+    //Getters e Setters
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutores() {
+        return autores;
+    }
+
+    public void setAutores(String autores) {
+        this.autores = autores;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
 }
